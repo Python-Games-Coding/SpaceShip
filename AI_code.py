31204f81fb7d3aa430912c2386bb5812cbfe9a53
MarsL422 <jiawei140820@hotmail.com>
2024-08-04 11:38:14 -0700

    Code File Update

---
+++ AI_code.py
@@ -0,0 +1,28 @@
+#------------------------------------------------------------------Enemy AI Code-----------------------------------------------------------------------#
+from pygame import*
+from tkinter import*
+from os import*
+from sys import*
+from webbrowser import*
+
+class MainAI:
+    def __init__(self):
+        pass
+
+class Learn(MainAI):
+    def __init__(self):
+        pass
+#------------------------------------------------------------------Rocket AI Code-----------------------------------------------------------------------#
+from pygame import*
+from tkinter import*
+from os import*
+from sys import*
+from webbrowser import*
+
+class MainAI:
+    def __init__(self):
+        pass
+
+class Learn(MainAI):
+    def __init__(self):
+        pass
